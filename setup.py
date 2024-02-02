a15b3ebf98a14a2f9e70808aab542533673c06e7
owais3901 <siddiquiowais390@gmail.com>
2024-02-02 18:42:13 +0500

    initial

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="VecDream",
+    version="0.0.1",
+    packages=find_packages(),
+    install_requires=[
+        "numpy",
+        "sentence_transformers",
+        # Add other dependencies as needed
+    ],
+    entry_points={
+        "console_scripts": [
+            "my_fastapi_project=VecDrean",
+        ],
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
